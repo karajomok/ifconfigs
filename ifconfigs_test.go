147f82c9f4b648452e1f24eea5227aadcdae5e69
karajomok <karajomok@gmail.com>
2024-12-08 06:52:07 +0300

    Add tests for freebsd parser function

--- ifconfigs_test.go
+++ ifconfigs_test.go
@@ -20,3 +20,28 @@ func Test_NetmaskHexToPrefix(t *testing.T) {
 	}
 
 }
+
+func Test_freebsdParser(t *testing.T) {
+	res, err := freebsdParser("testdata/test-ifconfig-output.txt")
+	if err != nil {
+		t.Error(err)
+	} else if res[0].Name != "vlan10" {
+		t.Errorf("NetworkInterface.Name must be <vlan10>, you got <%v>", res[0].Name)
+	} else if res[0].Status != "active" {
+		t.Errorf("NetworkInterface.Status must be <active>, you got <%v>", res[0].Status)
+	} else if res[0].Parent != "igb1" {
+		t.Errorf("NetworkInterface.Parent must be <igb1>, you got <%v>", res[0].Parent)
+	} else if res[0].MAC != "b4:2e:99:69:71:1d" {
+		t.Errorf("NetworkInterface.MAC must be <b4:2e:99:69:71:1d>, you got <%v>", res[0].MAC)
+	} else if res[0].MTU != "1500" {
+		t.Errorf("NetworkInterface.MTU must be <1500>, you got <%v>", res[0].MTU)
+	} else if res[0].Description != "mgt_switches" {
+		t.Errorf("NetworkInterface.Description must be <mgt_switches>, you got <%v>", res[0].Description)
+	} else if res[0].IpAddresses[0].IP != "172.16.186.158/27" {
+		t.Errorf("NetworkInterface.IpAddresses[0].IP must be <172.16.186.158/27>, you got <%v>", res[0].IpAddresses[0].IP)
+	} else if res[0].IpAddresses[0].CARP != "MASTER" {
+		t.Errorf("NetworkInterface.IpAddresses[0].CARP must be <MASTER>, you got <%v>", res[0].IpAddresses[0].CARP)
+	} else if res[0].IpAddresses[0].VHID != "51" {
+		t.Errorf("NetworkInterface.IpAddresses[0].Vhid must be <51>, you got <%v>", res[0].IpAddresses[0].VHID)
+	}
+}
