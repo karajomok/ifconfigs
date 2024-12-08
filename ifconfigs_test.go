31c3bc85286aa8339fcd9e75273314acea8bec26
karajomok <karajomok@gmail.com>
2024-12-09 00:27:24 +0300

    fix: expand tests for freebsd parser

--- ifconfigs_test.go
+++ ifconfigs_test.go
@@ -1,6 +1,8 @@
 package ifconfigs
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_NetmaskHexToPrefix(t *testing.T) {
 	exampleOne := "0xffffffc0"
@@ -22,26 +24,52 @@ func Test_NetmaskHexToPrefix(t *testing.T) {
 }
 
 func Test_freebsdParser(t *testing.T) {
-	res, err := freebsdParser("testdata/test-ifconfig-output.txt")
+	ifconfig, err := GetIfconfigData()
 	if err != nil {
 		t.Error(err)
-	} else if res[0].Name != "vlan10" {
-		t.Errorf("NetworkInterface.Name must be <vlan10>, you got <%v>", res[0].Name)
-	} else if res[0].Status != "active" {
-		t.Errorf("NetworkInterface.Status must be <active>, you got <%v>", res[0].Status)
-	} else if res[0].Parent != "igb1" {
-		t.Errorf("NetworkInterface.Parent must be <igb1>, you got <%v>", res[0].Parent)
-	} else if res[0].MAC != "b4:2e:99:69:71:1d" {
-		t.Errorf("NetworkInterface.MAC must be <b4:2e:99:69:71:1d>, you got <%v>", res[0].MAC)
-	} else if res[0].MTU != "1500" {
-		t.Errorf("NetworkInterface.MTU must be <1500>, you got <%v>", res[0].MTU)
-	} else if res[0].Description != "mgt_switches" {
-		t.Errorf("NetworkInterface.Description must be <mgt_switches>, you got <%v>", res[0].Description)
-	} else if res[0].IpAddresses[0].IP != "172.16.186.158/27" {
-		t.Errorf("NetworkInterface.IpAddresses[0].IP must be <172.16.186.158/27>, you got <%v>", res[0].IpAddresses[0].IP)
-	} else if res[0].IpAddresses[0].CARP != "MASTER" {
-		t.Errorf("NetworkInterface.IpAddresses[0].CARP must be <MASTER>, you got <%v>", res[0].IpAddresses[0].CARP)
-	} else if res[0].IpAddresses[0].VHID != "51" {
-		t.Errorf("NetworkInterface.IpAddresses[0].Vhid must be <51>, you got <%v>", res[0].IpAddresses[0].VHID)
+	}
+
+	res, err := freebsdParser(ifconfig)
+	if err != nil {
+		t.Error(err)
+	}
+
+	iface := res[0]
+	if iface.Name != "vlan10" {
+		t.Errorf("NetworkInterface.Name must be <vlan10>, you got <%v>", iface.Name)
+	} else if iface.Status != "active" {
+		t.Errorf("NetworkInterface.Status must be <active>, you got <%v>", iface.Status)
+	} else if iface.Type != "vlan" {
+		t.Errorf("NetworkInterface.Type must be <vlan>, you got <%v>", iface.Type)
+	} else if iface.VlanID != "10" {
+		t.Errorf("NetworkInterface.VlanID must be <10>, you got <%v>", iface.Type)
+	} else if iface.Parent != "igb1" {
+		t.Errorf("NetworkInterface.Parent must be <igb1>, you got <%v>", iface.Parent)
+	} else if iface.MAC != "b4:2e:99:69:71:1d" {
+		t.Errorf("NetworkInterface.MAC must be <b4:2e:99:69:71:1d>, you got <%v>", iface.MAC)
+	} else if iface.MTU != "1500" {
+		t.Errorf("NetworkInterface.MTU must be <1500>, you got <%v>", iface.MTU)
+	} else if iface.Description != "mgt_switches" {
+		t.Errorf("NetworkInterface.Description must be <mgt_switches>, you got <%v>", iface.Description)
+	}
+
+	// Check IPv4
+	ipv4 := iface.IpAddresses[0]
+	if ipv4.IP != "198.51.100.30/27" {
+		t.Errorf("ipv4.IP must be <198.51.100.30/27>, you got <%v>", ipv4.IP)
+	} else if ipv4.CARP != "MASTER" {
+		t.Errorf("ipv4.CARP must be <MASTER>, you got <%v>", ipv4.CARP)
+	} else if ipv4.VHID != "51" {
+		t.Errorf("ipv4.Vhid must be <51>, you got <%v>", ipv4.VHID)
+	}
+
+	// Check IPv6
+	ipv6 := iface.IpAddresses[1]
+	if ipv6.IP != "2001:db8:beef:305::1/64" {
+		t.Errorf("ipv6.IP must be <2001:db8:beef:305::1/64>, you got <%v>", ipv6.IP)
+	} else if ipv6.CARP != "BACKUP" {
+		t.Errorf("ipv6.CARP must be <BACKUP>, you got <%v>", ipv6.CARP)
+	} else if ipv6.VHID != "181" {
+		t.Errorf("ipv6.VHID must be <181>, you got <%v>", ipv6.VHID)
 	}
 }
