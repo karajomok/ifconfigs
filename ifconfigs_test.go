9d9b624019fcde27b266d5a5cd033ac1c25b90e5
karajomok <karajomok@gmail.com>
2024-12-09 04:53:41 +0300

    fix: rewrite and expand tests, change test data

--- ifconfigs_test.go
+++ ifconfigs_test.go
@@ -24,52 +24,112 @@ func Test_NetmaskHexToPrefix(t *testing.T) {
 }
 
 func Test_freebsdParser(t *testing.T) {
-	ifconfig, err := GetIfconfigData()
+	ifconfig, err := GetOutput()
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
 	res, err := freebsdParser(ifconfig)
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
-	iface := res[0]
-	if iface.Name != "vlan10" {
-		t.Errorf("NetworkInterface.Name must be <vlan10>, you got <%v>", iface.Name)
-	} else if iface.Status != "active" {
-		t.Errorf("NetworkInterface.Status must be <active>, you got <%v>", iface.Status)
-	} else if iface.Type != "vlan" {
-		t.Errorf("NetworkInterface.Type must be <vlan>, you got <%v>", iface.Type)
-	} else if iface.VlanID != "10" {
-		t.Errorf("NetworkInterface.VlanID must be <10>, you got <%v>", iface.Type)
-	} else if iface.Parent != "igb1" {
-		t.Errorf("NetworkInterface.Parent must be <igb1>, you got <%v>", iface.Parent)
-	} else if iface.MAC != "b4:2e:99:69:71:1d" {
-		t.Errorf("NetworkInterface.MAC must be <b4:2e:99:69:71:1d>, you got <%v>", iface.MAC)
-	} else if iface.MTU != "1500" {
-		t.Errorf("NetworkInterface.MTU must be <1500>, you got <%v>", iface.MTU)
-	} else if iface.Description != "mgt_switches" {
-		t.Errorf("NetworkInterface.Description must be <mgt_switches>, you got <%v>", iface.Description)
+	var iface NetworkInterface
+	var ips IPinfo
+
+	// vlan10
+	iface = res[0]
+	if res := iface.Name; res != "vlan10" {
+		t.Errorf("value must be <vlan10>, you got <%v>", res)
+	} else if res := iface.Status; res != "up" {
+		t.Errorf("value must be <up>, you got <%v>", res)
+	} else if res := iface.Type; res != "vlan" {
+		t.Errorf("value must be <vlan>, you got <%v>", res)
+	} else if res := iface.VlanID; res != "10" {
+		t.Errorf("value must be <10>, you got <%v>", res)
+	} else if res := iface.Parent; res != "igb1" {
+		t.Errorf("value must be <igb1>, you got <%v>", res)
+	} else if res := iface.MAC; res != "b4:2e:99:69:71:1d" {
+		t.Errorf("value must be <b4:2e:99:69:71:1d>, you got <%v>", res)
+	} else if res := iface.MTU; res != "1500" {
+		t.Errorf("value must be <1500>, you got <%v>", res)
+	} else if res := iface.Description; res != "mgt_switches" {
+		t.Errorf("value must be <mgt_switches>, you got <%v>", res)
 	}
 
 	// Check IPv4
-	ipv4 := iface.IpAddresses[0]
-	if ipv4.IP != "198.51.100.30/27" {
-		t.Errorf("ipv4.IP must be <198.51.100.30/27>, you got <%v>", ipv4.IP)
-	} else if ipv4.CARP != "MASTER" {
-		t.Errorf("ipv4.CARP must be <MASTER>, you got <%v>", ipv4.CARP)
-	} else if ipv4.VHID != "51" {
-		t.Errorf("ipv4.Vhid must be <51>, you got <%v>", ipv4.VHID)
+	if len(iface.IpAddresses) != 4 {
+		t.Error("IP addresses for vlan10 not found, must be 4 addresses")
+		return
+	}
+	ips = iface.IpAddresses[0]
+	if res := ips.IP; res != "198.51.3.30/27" {
+		t.Errorf("value must be <198.51.3.30/27>, you got <%v>", res)
+	} else if res := ips.CARP; res != "MASTER" {
+		t.Errorf("value must be <MASTER>, you got <%v>", res)
+	} else if res := ips.VHID; res != "51" {
+		t.Errorf("value must be <51>, you got <%v>", res)
+	} else if res := ips.Advbase; res != "2" {
+		t.Errorf("value must be <2>, you got <%v>", res)
+	} else if res := ips.Advskew; res != "50" {
+		t.Errorf("value must be <50>, you got <%v>", res)
+	}
+	// Check IPv4 without CARP
+	ips = iface.IpAddresses[1]
+	if res := ips.IP; res != "192.0.0.254/24" {
+		t.Errorf("value must be <192.0.0.254/24>, you got <%v>", res)
 	}
 
 	// Check IPv6
-	ipv6 := iface.IpAddresses[1]
-	if ipv6.IP != "2001:db8:beef:305::1/64" {
-		t.Errorf("ipv6.IP must be <2001:db8:beef:305::1/64>, you got <%v>", ipv6.IP)
-	} else if ipv6.CARP != "BACKUP" {
-		t.Errorf("ipv6.CARP must be <BACKUP>, you got <%v>", ipv6.CARP)
-	} else if ipv6.VHID != "181" {
-		t.Errorf("ipv6.VHID must be <181>, you got <%v>", ipv6.VHID)
+	ips = iface.IpAddresses[2]
+	if res := ips.IP; res != "2001:db8:beef:305::1/64" {
+		t.Errorf("value must be <2001:db8:beef:305::1/64>, you got <%v>", res)
+	} else if res := ips.CARP; res != "BACKUP" {
+		t.Errorf("value must be <BACKUP>, you got <%v>", res)
+	} else if res := ips.VHID; res != "181" {
+		t.Errorf("value must be <181>, you got <%v>", res)
+	} else if res := ips.Advbase; res != "2" {
+		t.Errorf("value must be <2>, you got <%v>", res)
+	} else if res := ips.Advskew; res != "155" {
+		t.Errorf("value must be <155>, you got <%v>", res)
 	}
+	// Check IPv6 without CARP
+	ips = iface.IpAddresses[3]
+	if res := ips.IP; res != "2001:db8:bd:91::1/64" {
+		t.Errorf("value must be <2001:db8:bd:91::1/64>, you got <%v>", res)
+	}
+
+	// ipsec99
+	iface = res[1]
+	if res := iface.Name; res != "ipsec99" {
+		t.Errorf("value must be <ipsec99>, you got <%v>", res)
+	} else if res := iface.Type; res != "ipsec" {
+		t.Errorf("value must be <ipsec>, you got <%v>", res)
+	} else if res := iface.MTU; res != "1500" {
+		t.Errorf("value must be <1500>, you got <%v>", res)
+	} else if res := iface.Description; res != "tunnel hostA -> hostB" {
+		t.Errorf("value must be <tunnel hostA -> hostB>, you got <%v>", res)
+	}
+
+	// Check IPs
+	if len(iface.IpAddresses) != 3 {
+		t.Error("IP addresses for ipsec not found, must be 4 addresses")
+		return
+	}
+
+	ips = iface.IpAddresses[0]
+	if res := ips.IP; res != "tunnel 10.0.0.1 --> 172.16.0.18" {
+		t.Errorf("value must be <tunnel 10.0.0.1 --> 172.16.0.18>, you got <%v>", res)
+	}
+	ips = iface.IpAddresses[1]
+	if res := ips.IP; res != "198.51.100.0 --> 198.51.100.1" {
+		t.Errorf("value must be <198.51.100.0 --> 198.51.100.1>, you got <%v>", res)
+	}
+	ips = iface.IpAddresses[2]
+	if res := ips.IP; res != "2001:db8:ada:4::2 --> 2001:db8:ada:4::1" {
+		t.Errorf("value must be <2001:db8:ada:4::2 --> 2001:db8:ada:4::1>, you got <%v>", res)
+	}
+
 }
