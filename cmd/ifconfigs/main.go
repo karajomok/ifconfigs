8e95b6412515d27c330342643ab112efa24088be
karajomok <karajomok@gmail.com>
2024-12-09 05:00:38 +0300

    feat: add json output

--- cmd/ifconfigs/main.go
+++ cmd/ifconfigs/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/karajomok/ifconfigs"
 )
 
 func main() {
-	ifconfig, err := ifconfigs.GetIfconfigData()
+	ifconfig, err := ifconfigs.GetOutput()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	interfaces := ifconfigs.ParseIfconfig(ifconfig)
-	fmt.Println(interfaces)
+	interfaces, err := ifconfigs.Parse(ifconfig)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := json.MarshalIndent(interfaces, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(res))
 }
